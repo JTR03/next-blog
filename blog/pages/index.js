dd15a8d7ca15b4797c2ffda223360bfebf7f3d65
Jonathan Tshikala <jonathanrose606@gmail.com>
2022-01-07 11:16:39 +0200

    completed

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Date from '../components/date'
 import Layout, {siteTitle} from '../components/layout'
+import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+  return{
+    props:{
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -13,6 +25,25 @@ export default function Home() {
         <p>
           Hi, I am Jonathan, I am an aspiring self taught software developer 
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id,date,title})=>(
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title} </a>
+              </Link>
+              
+              
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+              
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   
--- blog/pages/posts/[id].js
+++ blog/pages/posts/[id].js
@@ -1,8 +1,42 @@
 import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import styles from '../../styles/utils.module.css' 
+import Head from "next/head";
 
-export default function Post(){
+export async function getStaticPaths(){
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}){
+    const postData = await getPostData(params.id)
+    return{
+        props: {
+            postData
+        }
+    }
+}
+
+export default function Post({postData}){
     return(
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 className={styles.headingXl}>{postData.title}</h1>
+                <div className={styles.lightText}>
+                <Date dateString= {postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+            </article>
+            
+
+            
             
         </Layout>
     )
